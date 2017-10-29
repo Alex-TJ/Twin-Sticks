0cdb09be8bda43906dda90a040d862e6f18e78a6
Alex Tibulschi <tibulschia@outlook.com>
2017-10-29 20:08:55 +0000

    Added a simple menu system

---
+++ Assets/_Scenes/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest(){
+        Debug.Log("The user wants to quit!!!");
+        Application.Quit();
+    }
+}
