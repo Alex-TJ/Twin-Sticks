1a8c588f8971086e98c31df79dcaf3fabc40d39a
Alex Tibulschi <tibulschia@outlook.com>
2017-10-30 10:03:18 +0000

    Pausing Your game

--- Assets/Structure/GameManager.cs
+++ Assets/Structure/GameManager.cs
@@ -7,11 +7,16 @@ public class GameManager : MonoBehaviour {
 
 	public bool recording;
 
+	private bool paused;
+	private float fDelta;
+
 	// Use this for initialization
 	void Start () {
 		PlayerPrefsManager.UnlockLevel (2);
 		print (PlayerPrefsManager.IsLevelUnlocked(1));
 		print (PlayerPrefsManager.IsLevelUnlocked(2));
+
+		fDelta = Time.fixedDeltaTime;
 	}
 	
 	// Update is called once per frame
@@ -21,5 +26,27 @@ public class GameManager : MonoBehaviour {
 		} else {
 			recording = true;
 		}
+
+		if (Input.GetKeyDown(KeyCode.P) && !paused){
+			PauseGame ();
+			paused = true;
+		}else if (Input.GetKeyDown(KeyCode.P) && paused){
+			ResumeGame ();
+			paused = false;
+		}
+
+	}
+
+	void PauseGame(){
+
+		Time.timeScale = 0f;
+		Time.fixedDeltaTime = 0f;
+	}
+
+	void ResumeGame(){
+		
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = fDelta;
+	
 	}
 }
